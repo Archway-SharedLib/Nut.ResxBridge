5831a0664f2021f4ad9cecd87ff756e10b1162e9
kmaru <k-maru@archway.co.jp>
2021-04-11 13:15:05 +0900

    add xml doc comment.

--- src/Nut.ResxBridge/ResxClassTemplate.partial.cs
+++ src/Nut.ResxBridge/ResxClassTemplate.partial.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 
 namespace Nut.ResxBridge
@@ -17,5 +18,6 @@ namespace Nut.ResxBridge
         public string ArgsStr(ResxMemberCodeModel member) =>
             string.Join(", ", Enumerable.Range(0, member.ArgumentCount).Select(v => $"_{v}"));
 
+        public string EscXmlStr(string text) => SecurityElement.Escape(text);
     }
 }
---
+++ src/Nut.ResxBridge/Strings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Resources;
+
+namespace Nut.ResxBridge {
+
+    internal static class Strings {
+
+        private static ResourceManager? resourceMan;
+
+        internal static ResourceManager ResourceManager
+        {
+            get
+            {
+                if(resourceMan is null)
+                {
+                    resourceMan = new ResourceManager("Nut.ResxBridge.Strings", typeof(Strings).Assembly);
+                }
+                return resourceMan;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to {0}. に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string XmlDocCommentSumary(string text) => string.Format(ResourceManager.GetString("XmlDocCommentSumary"), text);
+    }
+}
